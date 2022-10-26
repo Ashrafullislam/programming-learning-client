8d42f9e2bd96623db04663a8810f80b719dc8f18
Md.Ashrafull Islam <ashrafull130@gmail.com>
2022-10-27 01:54:14 +0600

    added footer icon and copyright  messagee

--- src/Pages/SharedPage/FooterPart/Footer.js
+++ src/Pages/SharedPage/FooterPart/Footer.js
@@ -3,12 +3,23 @@ import {Col, Container, Row} from 'react-bootstrap';
 import {Link} from 'react-router-dom';
 import './Footer.css';
 
+import { BiEnvelope, IconName } from "react-icons/bi";
+import { FaAddressBook, FaCopyright, FaFacebook, FaInstagram, FaLinkedin, FaLocationArrow, FaPhone, FaTwitter } from 'react-icons/fa';
 const Footer = () => {
   return (
     <div className="footer">
-      <Container>
-        <Row>
-          <Col lg="6">
+      <Container className='py-5'>
+        <Row >
+         
+        <Col lg="4" >
+             <p className='footer-text'> <FaCopyright />  Copyright Warning </p>
+             <p className='footer-text'> <BiEnvelope /> email: ashrafull6252@gamil.com </p>
+             <p className='footer-text'> <FaLocationArrow  /> Address: Sylhet ,Banglades </p>
+             <p className='footer-text'> All rights reserved by Ashrafull  </p>
+              
+           </Col>
+
+          <Col lg="4">
             <div className="footer-option">
               <div>
                 <Link> Home </Link>
@@ -28,9 +39,17 @@ const Footer = () => {
             </div>
           </Col>
 
-          <Col lg="6" />
-        </Row>
-      </Container>
+          
+            <Col lg="4" >
+                <h5 className='text-light'> <FaPhone /> Contact us  </h5>
+                <p className='social-icon'> <Link> <FaFacebook /> Facebook </Link> </p>
+                <p className='social-icon'> <Link> <FaTwitter /> Twitter </Link> </p>
+                <p className='social-icon'> <Link> <FaLinkedin /> Linkedin </Link> </p>
+
+           </Col>
+           
+            </Row>
+        </Container>
 
     </div>
   );
